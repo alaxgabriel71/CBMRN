2c0fd1c82be4c3a1f0f82cafc359adabe335ada0
alaxgabriel71 <alaxgabriel71@gmail.com>
2022-10-26 11:07:09 -0300

    consuming API working

--- Almox/web/web_almox/src/pages/MaterialsList.js
+++ Almox/web/web_almox/src/pages/MaterialsList.js
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react'
+
+import api from '../services/api'
+
 export default function MaterialsList(){
+    const [materials, setMaterials] = useState([])
+
+    useEffect(() => {
+        api.get("/materials")
+        .then(({data}) => {
+            setMaterials(data.materials)
+        })
+        .catch((err) => {
+            console.log("deu erro: " + err)
+        });
+    }, [])
+    console.log(materials)
+
+    
     return(
         <div>
             <h2>Lista de Materiais</h2>
+            {materials?.map(material => <h3 key={material.id}>{material.name}</h3>)}
         </div>
     )
 }
