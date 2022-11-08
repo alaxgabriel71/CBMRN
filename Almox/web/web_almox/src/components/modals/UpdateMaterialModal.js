b651cd88fb8e81e924c4045f79c96ca3a7b240f5
Alax Gabriel <alaxgabriel71@gmail.com>
2022-11-07 23:24:52 -0300

    styling the update materials modal

--- Almox/web/web_almox/src/components/modals/UpdateMaterialModal.js
+++ Almox/web/web_almox/src/components/modals/UpdateMaterialModal.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import styles from './UpdateMaterialModal.module.css'
 import api from '../../services/api'
 import StatusMessage from '../StatusMessage'
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
 
 export default function UpdateMaterialModal({ show, onClose, materialName, materialId, materialQuantity }) {
     const [newName, setNewName] = useState(materialName);
@@ -13,8 +14,8 @@ export default function UpdateMaterialModal({ show, onClose, materialName, mater
 
     var dateObject = new Date()
     const formatedDate = dateObject.getDate() < 10 ? ('0' + dateObject.getDate()) : (dateObject.getDate())
-    const today = formatedDate+'/'+(dateObject.getMonth()+1)+'/'+dateObject.getFullYear()
-    const date = dateObject.getFullYear()+'-'+(dateObject.getMonth()+1)+'-'+formatedDate
+    const today = formatedDate + '/' + (dateObject.getMonth() + 1) + '/' + dateObject.getFullYear()
+    const date = dateObject.getFullYear() + '-' + (dateObject.getMonth() + 1) + '-' + formatedDate
 
     useEffect(() => {
         api.get("/materials")
@@ -30,23 +31,23 @@ export default function UpdateMaterialModal({ show, onClose, materialName, mater
     if (!show) {
         return null
     }
- 
+
     const updateMaterial = (event) => {
-        event.preventDefault()        
+        event.preventDefault()
         var materialExists = false;
         var currentMaterial = {
             id: '',
             name: '',
             quantity: 0
-        } 
-            
+        }
+
         var updatedMaterial = {
             name: '',
             quantity: ''
         };
 
         materials.forEach(material => {
-            if(material.name.toLowerCase() === newName.toLowerCase()) {
+            if (material.name.toLowerCase() === newName.toLowerCase()) {
                 materialExists = true
                 currentMaterial.name = material.name
                 currentMaterial.id = material._id
@@ -54,10 +55,10 @@ export default function UpdateMaterialModal({ show, onClose, materialName, mater
             }
         })
 
-        if(materialExists && (materialId !== currentMaterial.id)){
+        if (materialExists && (materialId !== currentMaterial.id)) {
             event.preventDefault();
             return console.log(`Não foi possível concluir a edição, pois o material ${newName} já existe na lista de materiais.`)
-        } 
+        }
 
         updatedMaterial = {
             name: newName[0].toUpperCase() + newName.substring(1).toLowerCase(),
@@ -66,7 +67,7 @@ export default function UpdateMaterialModal({ show, onClose, materialName, mater
 
         api.put(`/materials/${materialId}`, updatedMaterial)
             .then((response) => {
-                console.log("Update status: "+response.status)
+                console.log("Update status: " + response.status)
 
                 const description = `${materialQuantity}x ${materialName} foi(foram) atualizado(s) para ${newQuantity}x ${newName} em ${today}`
                 api.post('/movements', {
@@ -88,43 +89,40 @@ export default function UpdateMaterialModal({ show, onClose, materialName, mater
             .catch(err => console.error(err))
         setMessage(false)
     }
-    
+
     return (
         <>
             <div className={styles.modal}>
                 <div className={styles.modal_content}>
                     <div className={styles.modal_header}>
-                        <StatusMessage message={message} status={status}/>
+                        <StatusMessage message={message} status={status} />
                         <h4 className={styles.modal_title}>Editar Material</h4>
                     </div>
                     <div className={styles.modal_body}>
                         <form id="update-form" onSubmit={updateMaterial}>
-                            <label>
-                                Quantidade
-                                <input 
-                                    type="number" 
-                                    min="1" 
-                                    defaultValue={materialQuantity}
-                                    value={newQuantity}
-                                    onChange={event => setNewQuantity(event.target.value)}
-                                    required
-                                />
-                            </label>
-                            <label>
-                                Nome
-                                <input 
-                                    type="text" 
-                                    defaultValue={materialName}
-                                    value={newName}
-                                    onChange={event => setNewName(event.target.value)}
-                                    required 
-                                />
-                            </label>
+                            <FloatingLabel
+                                label="Quantidade"
+                                value={newQuantity}
+                                onChange={event => setNewQuantity(event.target.value)}
+                                required
+                                className={styles.QuantityFloatingLabel}
+                            >
+                                <Form.Control type="number" min="1" defaultValue={materialQuantity} />
+                            </FloatingLabel>
+                            <FloatingLabel
+                                label="Nome"
+                                value={newName}
+                                onChange={event => setNewName(event.target.value)}
+                                required
+                                className={styles.NameFloatingLabel}
+                            >
+                                <Form.Control type="text" defaultValue={materialName} />
+                            </FloatingLabel>
                         </form>
                     </div>
                     <div className={styles.modal_footer}>
-                        <button onClick={onClose} className={styles.button}>Cancelar</button>
-                        <button type="submit" form="update-form">Salvar Edição</button>
+                        <Button id="cancel" variant="secondary" onClick={onClose} className={styles.button}>Cancelar</Button>
+                        <Button id="confirm" variant="danger" type="submit" form="update-form">Salvar Edição</Button>
                     </div>
                 </div>
             </div>
