3ae38496a9ac14dd405bfad32c9d820539863400
alaxgabriel71 <alaxgabriel71@gmail.com>
2022-11-08 09:17:07 -0300

    Adding style to the take care material modal

--- Almox/web/web_almox/src/components/modals/TakeCareMaterialModal.js
+++ Almox/web/web_almox/src/components/modals/TakeCareMaterialModal.js
@@ -3,13 +3,14 @@ import { useState, useEffect } from 'react'
 import styles from './TakeCareMaterialModal.module.css'
 import api from '../../services/api'
 import StatusMessage from '../StatusMessage'
+import { Button, FloatingLabel, Form } from 'react-bootstrap'
 
-export default function TakeCareMaterialModal({show, onClose, materialId, materialName, materialQuantity}){
-    
+export default function TakeCareMaterialModal({ show, onClose, materialId, materialName, materialQuantity }) {
+
     var dateObject = new Date()
     const formatedDate = dateObject.getDate() < 10 ? ('0' + dateObject.getDate()) : (dateObject.getDate())
-    const today = dateObject.getFullYear()+'-'+(dateObject.getMonth()+1)+'-'+formatedDate
-    
+    const today = dateObject.getFullYear() + '-' + (dateObject.getMonth() + 1) + '-' + formatedDate
+
     const [takeCareQuantity, setTakeCareQuantity] = useState(1)
     const [options, setOptions] = useState([])
     const [military, setMilitary] = useState()
@@ -19,7 +20,7 @@ export default function TakeCareMaterialModal({show, onClose, materialId, materi
 
     useEffect(() => {
         api.get('/military')
-            .then(({data}) => {
+            .then(({ data }) => {
                 setOptions(data.military)
             })
             .catch(err => {
@@ -27,23 +28,23 @@ export default function TakeCareMaterialModal({show, onClose, materialId, materi
             })
     }, []);
 
-    if(!show) return null
+    if (!show) return null
 
     const takeCareMaterial = (e) => {
         e.preventDefault()
         const finalQuantity = Number(materialQuantity) - Number(takeCareQuantity);
-        const takeCareDate = date.slice(8,10)+'/'+date.slice(5,7)+'/'+date.slice(0,4)
+        const takeCareDate = date.slice(8, 10) + '/' + date.slice(5, 7) + '/' + date.slice(0, 4)
 
-        if(finalQuantity === 0){
+        if (finalQuantity === 0) {
             api.delete(`/materials/${materialId}`)
                 .then(response => console.log(response.status))
                 .then(() => {
                     const description = `${takeCareQuantity}x ${materialName} foi(foram) cautelado(s) pelo ${military} em ${takeCareDate}`
-                    
+
                     api.post('/movements', {
                         operation: "Cautela",
                         date: today,
-                        description 
+                        description
                     })
                         .then(response => {
                             console.log(response.status)
@@ -70,11 +71,11 @@ export default function TakeCareMaterialModal({show, onClose, materialId, materi
                 .then(response => console.log(response.status))
                 .then(() => {
                     const description = `${takeCareQuantity}x ${materialName} foi(foram) cautelado(s) pelo ${military} em ${takeCareDate}`
-                    
+
                     api.post('/movements', {
                         operation: "Cautela",
                         date: today,
-                        description 
+                        description
                     })
                         .then(response => {
                             console.log(response.status)
@@ -103,36 +104,41 @@ export default function TakeCareMaterialModal({show, onClose, materialId, materi
                 <div className={styles.modal_body}>
                     <h3>{materialName}</h3>
                     <form onSubmit={takeCareMaterial} id="take-care-form">
-                        <label>
-                            Quantidade:
-                            <input 
-                            type="number" 
-                            min="1" 
-                            max={materialQuantity} 
-                            defaultValue={materialQuantity}
+                        <FloatingLabel
+                            label="Quantidade"
                             value={takeCareQuantity}
                             onChange={e => setTakeCareQuantity(e.target.value)}
                             required
+                            className={styles.QuantityFloatingLabel}
+                        >
+                            <Form.Control
+                                type="number"
+                                min="1"
+                                max={materialQuantity}
+                                defaultValue={materialQuantity}
                             />
-                        </label>
-                        <label>
-                            Cautelado por: 
-                            <select name="military" value={military} onChange={event => setMilitary(event.target.value)} required>
+                        </FloatingLabel>
+                        <FloatingLabel label="Cautelado por:" className={styles.MilitaryFloatingLabel}>
+                            <Form.Select value={military} onChange={e => setMilitary(e.target.value)}>
                                 <option value="" key="0">-- Selecione um militar --</option>
-                                {options?.map(option => 
+                                {options?.map(option =>
                                     <option key={option._id} value={option.name}>{option.name}</option>
                                 )}
-                            </select>
-                        </label>
-                        <label>
-                            Em:
-                            <input type="date" defaultValue={date} max={today} onChange={(e) => setDate(e.target.value)}/>
-                        </label>
+                            </Form.Select>
+                        </FloatingLabel>
+                        <FloatingLabel label="Em:" className={styles.DateFloatingLabel}>
+                            <Form.Control
+                                type="date"
+                                defaultValue={date}
+                                max={today}
+                                onChange={e => setDate(e.target.value)}
+                            />
+                        </FloatingLabel>
                     </form>
                 </div>
                 <div className={styles.modal_footer}>
-                    <button onClick={onClose} className={styles.button}>Cancelar</button>
-                    <button type="submit" form="take-care-form" disabled={!military || !takeCareQuantity}>Confirmar cautela de material</button>
+                    <Button id="cancel" variant="secondary" onClick={onClose} className={styles.button}>Cancelar</Button>
+                    <Button id="confirm" variant="danger" type="submit" form="take-care-form" disabled={!military || !takeCareQuantity}>Confirmar cautela de material</Button>
                 </div>
             </div>
         </div>
