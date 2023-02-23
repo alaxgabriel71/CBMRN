0d1ac98a075f6eb7dba70b53215826924538e9f4
alaxgabriel71 <alaxgabriel71@gmail.com>
2023-02-23 10:45:54 -0300

    displaying the navigation's links according to the administrator level

--- Almox/web/web_almox/src/components/NavBar.jsx
+++ Almox/web/web_almox/src/components/NavBar.jsx
@@ -65,10 +65,10 @@ function NavBar() {
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
                   <Nav.Link href="/">Página Inicial</Nav.Link>
-                  <Nav.Link href="/materials-tabs">Materiais</Nav.Link>
-                  <Nav.Link href="/movement-history">Histórico de movimentações</Nav.Link>
+                  <Nav.Link hidden={!user|| !user.admin.commander && !user.admin.moderator && !user.admin.regular}href="/materials-tabs">Materiais</Nav.Link>
+                  <Nav.Link hidden={!user|| !user.admin.commander && !user.admin.moderator} href="/movement-history">Histórico de movimentações</Nav.Link>
                   <Nav.Link href="/military-tabs">Militares</Nav.Link>
-                  <Nav.Link hidden={!user.admin.commander} href="/register">Cadastrar novo usuário</Nav.Link>
+                  <Nav.Link hidden={!user|| !user.admin.commander} href="/register">Cadastrar novo usuário</Nav.Link>
                   {/* <NavDropdown
                   title="Dropdown"
                   id={`offcanvasNavbarDropdown-expand-${expand}`}
