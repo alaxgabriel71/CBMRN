1b91c6c615f563f65b222e79ea5e6108c9d861d6
alaxgabriel71 <alaxgabriel71@gmail.com>
2023-03-09 12:10:12 -0300

    creating a page to the command see the users

---
+++ Almox/web/web_almox/src/pages/Users.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState, useContext } from 'react'
+
+import { UserContext } from '../components/contexts/UserContext'
+import api from '../services/api'
+
+export default function Users() {
+
+    const [users, setUsers] = useState([])
+
+    const { user } = useContext(UserContext)
+
+    api.defaults.headers.Authorization = `Bearer ${user.token}`
+
+    api.get('/users')
+        .then(({data}) => {
+            setUsers(data.users)
+        })
+        .catch(err => {
+            console.error(err.message)
+        })
+
+    return (
+        <article>
+            <h1>Usuários</h1>
+            <ul>
+                {users.map(user => (
+                    <li key={user._id}>{user.name} - {user.email}</li>
+                ))}
+            </ul>
+        </article>
+    )
+}
